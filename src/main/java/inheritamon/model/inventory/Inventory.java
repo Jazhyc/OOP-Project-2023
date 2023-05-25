a9b202dcb0936ea0f5769a8f63f51c441dc9a758
JonaJanssen <jonajanssenofficial@gmail.com>
2023-05-25 21:10:40 +0200

    Initial inventory setup

---
+++ src/main/java/inheritamon/model/inventory/Inventory.java
@@ -0,0 +1,22 @@
+package inheritamon.model.inventory;
+
+public interface Inventory {
+    private int size;
+    private ArrayList items;
+
+    public int getSize() {
+    }
+
+    public void setSize(int newSize) {
+    }
+
+    public void addItem() {
+    }
+
+    public void removeItem() {
+    }
+
+
+
+}
+
