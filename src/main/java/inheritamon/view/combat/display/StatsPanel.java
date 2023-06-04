878f405ee48c5cc54c34b2d1cf4a4509497cb787
Jeremias Ferrao <feronest@yahoo.com>
2023-06-04 13:32:33 +0200

    Added animation to stat bars

--- src/main/java/inheritamon/view/combat/display/StatsPanel.java
+++ src/main/java/inheritamon/view/combat/display/StatsPanel.java
@@ -5,14 +5,16 @@ import inheritamon.model.BattleHandler;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.lang.reflect.*;
 
 
 public class StatsPanel extends JPanel {
     
     // Set default values to represent a full bar
     // The user won't notice
-    private float hpRatio = 1;
-    private float mpRatio = 1;
+    private float HPRatio = 1;
+    private float MPRatio = 1;
 
     private int yOffset = 10;
     private int barWidth = 300;
@@ -51,9 +53,9 @@ public class StatsPanel extends JPanel {
 
         // Draw the bars
         g.setColor(Color.GREEN);
-        g.fillRect(hpBarCenterX, centerY - barHeight + yOffset, (int) (barWidth * hpRatio), barHeight);
+        g.fillRect(hpBarCenterX, centerY - barHeight + yOffset, (int) (barWidth * HPRatio), barHeight);
         g.setColor(Color.BLUE);
-        g.fillRect(mpBarCenterX, centerY + yOffset, (int) (barWidth * mpRatio), (barHeight));
+        g.fillRect(mpBarCenterX, centerY + yOffset, (int) (barWidth * MPRatio), (barHeight));
 
         // Add labels HP and MP to the bars
         g.setColor(Color.BLACK);
@@ -77,8 +79,12 @@ public class StatsPanel extends JPanel {
                 int[] stats = (int[]) e.getNewValue();
                 
                 // Calculate the ratios, let the minimum be 0
-                hpRatio = Math.max((float) stats[0] / stats[1], 0);
-                mpRatio = Math.max((float) stats[2] / stats[3], 0);
+                float newHPRatio = Math.max((float) stats[0] / stats[1], 0);
+                float newMPRatio = Math.max((float) stats[2] / stats[3], 0);
+
+                // Tween the values
+                tweenValue(HPRatio, newHPRatio, 30, "HPRatio");
+                tweenValue(MPRatio, newMPRatio, 30, "MPRatio");
 
                 // Repaint the panel
                 revalidate();
@@ -89,4 +95,55 @@ public class StatsPanel extends JPanel {
         });
     }
 
+    /**
+     * Tween a value from one value to another over a duration
+     * @param oldValue The old value
+     * @param newValue The new value
+     * @param duration The duration of the tween
+     * @param attributeName The name of the attribute to tween, uses reflection for versatility
+     */
+    private void tweenValue(float oldValue, float newValue, int duration, String attributeName) {
+        // Calculate the difference between the values
+        float difference = newValue - oldValue;
+
+        // Calculate the amount to change the value by each frame
+        float changePerFrame = difference / duration;
+
+        // Create a new timer to tween the value
+        Timer timer = new Timer(1, null);
+        timer.addActionListener(new ActionListener() {
+            float incrementValue = oldValue;
+            int elapsed = 0;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Add the change per frame to the old value
+                incrementValue += changePerFrame;
+
+                // Set the attribute value using reflection
+                try {
+                    Field field = StatsPanel.this.getClass().getDeclaredField(attributeName);
+                    field.setAccessible(true);
+                    field.set(StatsPanel.this, incrementValue);
+                } catch (NoSuchFieldException | IllegalAccessException ex) {
+                    ex.printStackTrace();
+                }
+
+                // Repaint the panel
+                revalidate();
+                repaint();
+
+                // Increment the elapsed time
+                elapsed++;
+
+                // Stop the timer if the duration has elapsed
+                if (elapsed >= duration) {
+                    timer.stop();
+                }
+            }
+        });
+
+        // Start the timer
+        timer.start();
+    }
 }
