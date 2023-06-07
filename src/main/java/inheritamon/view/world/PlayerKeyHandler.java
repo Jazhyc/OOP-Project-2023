d11521b9d1f77a56269e9d669335ad8e9e383824
Jeremias Ferrao <feronest@yahoo.com>
2023-06-07 14:06:22 +0200

    Side panel now restricts movement when visible

--- src/main/java/inheritamon/view/world/PlayerKeyHandler.java
+++ src/main/java/inheritamon/view/world/PlayerKeyHandler.java
@@ -2,110 +2,92 @@ package inheritamon.view.world;
 
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.*;
 
 public class PlayerKeyHandler {
 
     private boolean upPressed, downPressed, leftPressed, rightPressed;
 
+    SidePanel sidePanel;
+
+    
     public PlayerKeyHandler(JComponent component, SidePanel sidePanel) {
         InputMap inputMap = component.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
         ActionMap actionMap = component.getActionMap();
 
-        // Bind the "W" and "UP" keys to the "up" action
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0), "up pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "up pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, true), "up released");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0, true), "up released");
-        actionMap.put("up pressed", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                upPressed = true;
-            }
-        });
-        actionMap.put("up released", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                upPressed = false;
-            }
-        });
+        this.sidePanel = sidePanel;
 
-        // Bind the "S" and "DOWN" keys to the "down" action
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0), "down pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "down pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0, true), "down released");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0, true), "down released");
-        actionMap.put("down pressed", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                downPressed = true;
-            }
-        });
-        actionMap.put("down released", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                downPressed = false;
-            }
-        });
+        // Define the key bindings and actions
+        Map<Integer, String> keyBindings = new HashMap<>();
+        keyBindings.put(KeyEvent.VK_W, "up");
+        keyBindings.put(KeyEvent.VK_S, "down");
+        keyBindings.put(KeyEvent.VK_A, "left");
+        keyBindings.put(KeyEvent.VK_D, "right");
+        keyBindings.put(KeyEvent.VK_ESCAPE, "escape");
 
-        // Bind the "A" and "LEFT" keys to the "left" action
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0), "left pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "left pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true), "left released");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0, true), "left released");
-        actionMap.put("left pressed", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                leftPressed = true;
-            }
-        });
-        actionMap.put("left released", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                leftPressed = false;
-            }
-        });
+        // Bind the keys to their corresponding actions using a loop
+        for (Map.Entry<Integer, String> entry : keyBindings.entrySet()) {
+            int keyCode = entry.getKey();
+            String actionName = entry.getValue();
 
-        // Bind the "D" and "RIGHT" keys to the "right" action
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0), "right pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "right pressed");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true), "right released");
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0, true), "right released");
-        actionMap.put("right pressed", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                rightPressed = true;
-            }
-        });
-        actionMap.put("right released", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                rightPressed = false;
-            }
-        });
+            // Bind the key press and release events to their corresponding actions
+            inputMap.put(KeyStroke.getKeyStroke(keyCode, 0), actionName + " pressed");
+            inputMap.put(KeyStroke.getKeyStroke(keyCode, 0, true), actionName + " released");
 
-        // If escape is pressed, change the visibility of the side panel
-        inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "escape pressed");
-        actionMap.put("escape pressed", new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                sidePanel.setVisible(!sidePanel.isVisible());
-            }
-        });
+            // Define the actions for the key press and release events
+            actionMap.put(actionName + " pressed", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    setKeyState(actionName, true);
+                }
+            });
+            actionMap.put(actionName + " released", new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                    // Ignore if escape is released
+                    if (actionName.equals("escape")) {
+                        return;
+                    }
+
+                    setKeyState(actionName, false);
+                }
+            });
+        }
     }
 
+private void setKeyState(String actionName, boolean state) {
+    switch (actionName) {
+        case "up":
+            upPressed = state;
+            break;
+        case "down":
+            downPressed = state;
+            break;
+        case "left":
+            leftPressed = state;
+            break;
+        case "right":
+            rightPressed = state;
+            break;
+        case "escape":
+            sidePanel.setVisible(!sidePanel.isVisible());
+            break;
+    }
+}
     public boolean isUpPressed() {
-        return upPressed;
+        return !sidePanel.isVisible() && upPressed;
     }
 
     public boolean isDownPressed() {
-        return downPressed;
+        return !sidePanel.isVisible() && downPressed;
     }
 
     public boolean isLeftPressed() {
-        return leftPressed;
+        return !sidePanel.isVisible() && leftPressed;
     }
 
     public boolean isRightPressed() {
-        return rightPressed;
+        return !sidePanel.isVisible() && rightPressed;
     }
 }
