6d5d7b38568cf7f8849b1ac40c4aee372cda49f3
Jeremias Ferrao <feronest@yahoo.com>
2023-06-11 03:20:56 +0200

    Added language change functionality to classDisplay

--- src/main/java/inheritamon/view/menu/ClassDisplayPanel.java
+++ src/main/java/inheritamon/view/menu/ClassDisplayPanel.java
@@ -2,25 +2,54 @@ package inheritamon.view.menu;
 
 import javax.swing.*;
 
-public class ClassDisplayPanel extends JPanel {
+import inheritamon.model.data.language.*;
+
+public class ClassDisplayPanel extends JPanel implements LanguageChangeListener {
+
+    private JLabel[] labels = new JLabel[3];
+
+    private String perk;
 
     public ClassDisplayPanel(String pokemon, String perk) {
 
+        this.perk = perk;
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
+        LanguageConfiguration config = LanguageConfiguration.getInstance();
+
         JLabel pokemonLabel = new JLabel(pokemon);
-        JLabel perkLabel = new JLabel(perk);
+        JLabel perkLabel = new JLabel(config.getText(perk));
+        JLabel perkDescription = new JLabel(config.getText(perk + "Description"));
+
+        // Add the labels to the array
+        labels[0] = pokemonLabel;
+        labels[1] = perkLabel;
+        labels[2] = perkDescription;
 
         // Put the labels in the center of the panel
         pokemonLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         perkLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-
-        // Make the labels occupy the entire width of the panel
-        pokemonLabel.setMaximumSize(pokemonLabel.getPreferredSize());
-        perkLabel.setMaximumSize(perkLabel.getPreferredSize());
+        perkDescription.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
         add(pokemonLabel);
         add(perkLabel);
+        add(perkDescription);
+
+        addLanguageListener();
+
+    }
+
+    @Override
+    public void addLanguageListener() {
+
+        LanguageConfiguration config = LanguageConfiguration.getInstance();
+
+        config.addLanguageChangeListener(e -> {
+            // Update the labels
+            labels[1].setText(config.getText(perk));
+            labels[2].setText(config.getText(perk + "Description"));
+        });
 
     }
     
